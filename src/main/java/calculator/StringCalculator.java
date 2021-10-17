5a770194950ac4ccf2bcc39bd7c5109a8a4e6d8e
Sanket <Sanket@LAPTOP-2OCT9C7I>
2021-10-17 23:09:46 +0530

    commit 4

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -10,18 +10,42 @@ class StringCalculator {
     	else 
     	{
     		int sum = 0;
-    		if(input.contains(",")) 
+    		if(input.contains(",") && !input.contains("\n")
+) 
     		{
     			String s[] = input.split(",");
     			for(int i = 0 ; i< s.length ; i++)
     			{
     				sum += Integer.valueOf(s[i]);	
-    			}
-    			
+    			}	
     		}
-    		else
+    		else if(!input.contains(","))
     		{
     			sum = Integer.valueOf(input);
+    		}
+    		else 
+    		{
+    			String temp = input.replace("\n", "");
+    			String s[] = temp.split(",");
+    			
+    			for(int i = 0 ; i< s.length ; i++)
+    			{
+    				if(!s[i].isEmpty() && Integer.valueOf(s[i]) <10) 
+    				{	
+    					sum += Integer.valueOf(s[i]);
+    				}
+    				else 
+    				{
+    					int tem = Integer.valueOf(s[i]);
+    					int rem=0;
+    					while(tem > 0) 
+    					{
+    						rem = tem%10;
+    						sum += rem;
+    						tem = tem/10;
+    					}		
+    				}	
+    			}
     		}
     		return sum;
     	}
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -4,23 +4,41 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringCalculatorShould {
 
+class StringCalculatorShould 
+{
     @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
+    public void empty_string_should_return_0() 
+    {
+    	StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
+    public void string_with_single_number_should_return_number_as_int()
+    {
+    	StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
     
     @Test
-    void string_with_double_number_should_return_number_as_sum() {
-        StringCalculator stringCalculator = new StringCalculator();
+    public void string_with_double_number_should_return_number_as_sum() 
+    {
+    	StringCalculator stringCalculator = new StringCalculator();
         assertEquals(3, stringCalculator.add("1,2"));
+    }
+    
+    @Test
+    public void string_with_unknown_number_should_return_number_as_sum() 
+    {
+    	StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(32+52+45+85+20, stringCalculator.add("32,52,45,85,20"));
+    }
+    
+    @Test
+    public void string_with_newLine_between_two_number_should_return_number_as_sum() 
+    {
+    	StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(6, stringCalculator.add("1\n2,3"));
     }
 }
