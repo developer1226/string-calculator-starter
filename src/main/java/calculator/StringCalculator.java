224a0d7aff700945e63cbd8ae15b3d965501abef
Sanket <Sanket@LAPTOP-2OCT9C7I>
2021-10-17 20:54:00 +0530

    commit 2

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,11 +2,29 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-    	if(input.isEmpty())	
+	public int add(String input) {
+    	if(input.isEmpty())
+    	{
     		return 0;
-    	//if(!input.isEmpty())
-    	return Integer.parseInt(input);
+    	}
+    	else 
+    	{
+    		int sum = 0;
+    		if(input.contains(",")) 
+    		{
+    			String s[] = input.split(",");
+    			for(int i = 0 ; i< s.length ; i++)
+    			{
+    				sum += Integer.valueOf(s[i]);	
+    			}
+    			
+    		}
+    		else
+    		{
+    			sum = Integer.valueOf(input);
+    		}
+    		return sum;
+    	}
     }
 
 }
